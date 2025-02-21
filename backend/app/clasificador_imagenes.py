010b7023e50ba60efaff4e4445ca24becb2436e1
juanquirozt <juquirozt@unal.edu.co>
2025-02-21 02:30:24 -0500

    Pequeño cambio en algunos comentarios

--- backend/app/clasificador_imagenes.py
+++ backend/app/clasificador_imagenes.py
@@ -8,11 +8,11 @@ import os
 modelo_path = os.path.join(os.path.dirname(__file__), '../modelo/modelo_imagenes.h5')
 modelo = load_model(modelo_path)
 
-# Lista de clases (ajusta esto según tu modelo)
+# Lista de clases
 clases = ['Jeans', 'Sofa', 'Tshirt', 'Tv']
 
 def clasificar_imagen(imagen_path):
-    img = image.load_img(imagen_path, target_size=(224, 224), color_mode="grayscale")  # Ajusta el tamaño según el modelo
+    img = image.load_img(imagen_path, target_size=(224, 224), color_mode="grayscale")
     img_array = image.img_to_array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
