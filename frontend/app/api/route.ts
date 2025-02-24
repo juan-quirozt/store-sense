9b7897284fb9e9777d2e5e3802c81be4e84c223c
juanquirozt <juquirozt@unal.edu.co>
2025-02-23 19:34:19 -0500

    .

--- frontend/app/api/route.ts
+++ frontend/app/api/route.ts
@@ -1,6 +1,7 @@
 export async function POST(req: Request) {
     try {
       const formData = await req.formData();
+      console.log(Request)
   
       const response = await fetch("http://localhost:5000/api/clasificar", {
         method: "POST",
